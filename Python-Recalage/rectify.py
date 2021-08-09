6125b14e57b2c21ff00bf350ac4a786fb26db2f1
olivierdesclaux <desclaux.olivier@gmail.com>
2021-08-09 18:36:40 -0400

    Debugged rectify so that it runs

--- Python-Recalage/rectify.py
+++ Python-Recalage/rectify.py
@@ -12,23 +12,22 @@ from utils import scaleForHconcat, stop, openCalibrationFile
 from circledetector import CircleDetector
 from circlegridfinder import CircleGridFinder
 
-def main(cameraTypes, calibFile1, calibFile2):
 
+def main(cameraTypes, calibFileLeft, calibFileRight):
     # Open calibration files and read contents
-    matrix1, dist1 = openCalibrationFile(calibFile1)
-    matrix2, dist2 = openCalibrationFile(calibFile2)
+    matrixLeft, distLeft = openCalibrationFile(calibFileLeft)
+    matrixRight, distRight = openCalibrationFile(calibFileRight)
 
     # If empty, quit
-    if not dist1.any():
+    if not distLeft.any():
         sys.exit("Cannot read matrices from Kinect calibration file.")
-    if not dist2.any():
+    if not distRight.any():
         sys.exit("Cannot read matrices from FLIR calibration file.")
 
-
-    if "F" in cameraTypes and "K" in cameraTypes: # FLIR + Kinect
-        streams = openStreams(targetHeights=[720, 768])
-        types = [StreamType.rgb, StreamType.ir]
-    if "W" in cameraTypes and "K" in cameraTypes: # Webcam + Kinect
+    if "F" in cameraTypes and "K" in cameraTypes:  # FLIR + Kinect
+        streams = openStreams(targetHeights=[768, 720])
+        types = [StreamType.ir, StreamType.rgb]
+    elif "W" in cameraTypes and "K" in cameraTypes:  # Webcam + Kinect
         streams = openStreams(targetCameras=[1, 0])
         types = [StreamType.rgb, StreamType.rgb]
     else:
@@ -58,59 +57,77 @@ def main(cameraTypes, calibFile1, calibFile2):
                 frame = stream.read().copy()
                 gridFinder.drawOutlines([frame]) # Small performance impact, displays previously found grids as green lines
                 frames.append(frame)
-        scaleForHconcat(frames[1], frames[0], 0.75)
-        concatFrame = cv.hconcat([frames[0], frames[1]])
+
+        # Resizing and concatenating for imshow
+        resizedFrameLeft, resizedFrameRight = scaleForHconcat(frames[0], frames[1], 0.5)
+        concatFrame = cv.hconcat([resizedFrameLeft, resizedFrameRight])
         # Indicates how many images remain to take in upper left
-        cv.putText(concatFrame, 'Remaining images: ' + str(rectImageCount - gridFinder.len()), (10, 50), cv.FONT_HERSHEY_SIMPLEX, 1, (250, 150, 0), 2)
+        cv.putText(concatFrame, 'Remaining images: ' + str(rectImageCount - gridFinder.len()), (10, 50),
+                   cv.FONT_HERSHEY_SIMPLEX, 1, (250, 150, 0), 2)
         # Shows frame
         cv.imshow('Rectify', concatFrame)
+        if cv.waitKey(1) == ord('q'):
+            raise Exception("User exited program")
 
+    cv.destroyAllWindows()
     # Stereo calibration in OpenCV, using keypoints identified above by gridfinder
     # This obtains the R and T matrices to transform between 2 CAMERA POSITIONS, plus improved camera matrices and distortion coefficients
     # Uses previous calibration parameters for instrinsics (cv.CALIB_FIX_INTRINSIC)
-    frameSize1 = frames[0, :, :, 0].shape[::-1]
-    frameSize2 = frames[1, :, :, 0].shape[::-1]
-    stereoSuccess, matrix1, dist1, matrix2, dist2, R, T, E, F = cv.stereoCalibrate( 
-        gridFinder.objectPositions, gridFinder.allImagePositions[0], gridFinder.allImagePositions[1], 
-        matrix1, dist1, matrix2, dist2, frameSize1, flags=cv.CALIB_FIX_INTRINSIC, criteria=criteria)
-    
+    frameSizeLeft = frames[0][:, :, 0].shape[::-1]
+    frameSizeRight = frames[1][:, :, 0].shape[::-1]
+
+    stereoSuccess, matrixLeft, distLeft, matrixRight, distRight, R, T, E, F = cv.stereoCalibrate(
+        gridFinder.objectPositions, gridFinder.allImagePositions[0], gridFinder.allImagePositions[1],
+        matrixLeft, distLeft, matrixRight, distRight, frameSizeLeft, flags=cv.CALIB_FIX_INTRINSIC, criteria=criteria)
+
     if stereoSuccess:
         print("Stereo calibration successful.")
+        print("E: ", E)
+        print("F: ", F)
+        print("Matrix Left: ", matrixLeft)
+        print("Matrix Right: ", matrixRight)
+        print("dist Left: ", distLeft)
+        print("dist Right: ", distRight)
         # Alpha can be varied from 0 (no black pixels) to 1 (all pixels) in undistorted image
         # This obtains precise R and T matrices to transforms PIXELS between images
-        R1, R2, P1, P2, Q, roi1, roi2 = cv.stereoRectify(matrix1, dist1, matrix2, dist2, frameSize1, R, T, alpha=1)
+        RLeft, RRight, PLeft, PRight, Q, roiLeft, roiRight = cv.stereoRectify(matrixLeft, distLeft, matrixRight, distRight, frameSizeLeft, R, T, alpha=1)
         # Calculates maps for undistorting each image into a rectified state
-        mapX1, mapY1 = cv.initUndistortRectifyMap(matrix1, dist1, R1, P1, frameSize1, cv.CV_32FC1) #try CV_32FC2
-        mapX2, mapY2 = cv.initUndistortRectifyMap(matrix2, dist2, R2, P2, frameSize2, cv.CV_32FC1) #try CV_32FC2
+        mapXLeft, mapYLeft = cv.initUndistortRectifyMap(matrixLeft, distLeft, RLeft, PLeft, frameSizeLeft, cv.CV_32FC1)  # try CV_32FC2
+        mapXRight, mapYRight = cv.initUndistortRectifyMap(matrixRight, distRight, RRight, PRight, frameSizeRight, cv.CV_32FC1)  # try CV_32FC2
+
+    else:
+        raise Exception("Stereo calibration failed")
 
     # Displayed undistorted image for debugging, should be in another file!
     while True:
-        frame1 = streams[0].read()
-        frame2 = streams[1].read()
-        # Maps pixels from FLIR to Kinect image
-        frame2Remap = cv.remap(frame2, mapX2, mapY2, cv.INTER_LINEAR)
+        frameLeft = streams[0].read()
+        frameRight = streams[1].read()
+
+        # Remap both views
+        frameLeftRemap = cv.remap(frameLeft, mapXLeft, mapYLeft, cv.INTER_LINEAR)
+        frameRightRemap = cv.remap(frameRight, mapXRight, mapYRight, cv.INTER_LINEAR)
 
         # Scales images for display side-by-side
-        scaleForHconcat(frame2Remap, frame1, 0.75)
+        resizedFrameLeftRemap, resizedFrameRightRemap = scaleForHconcat(frameLeftRemap, frameRightRemap, 0.5)
 
         # Side-by-side display, requires both frames have same height
-        concatFrame = cv.hconcat([frame1, frame2Remap])
+        concatFrame = cv.hconcat([resizedFrameLeftRemap, resizedFrameRightRemap])
         cv.imshow('Undistorted', concatFrame)
 
         # Waits for next frame or quits main loop if 'q' is pressed
         if cv.waitKey(1) == ord('q'):
             stop("User exited program.")
-        
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', type=str, dest='cameraTypes', help='List of 2 cameras to open', required=True)
-    parser.add_argument('--calib1', type=str, dest='calibFile1', help='First file to open', required=True)
-    parser.add_argument('--calib2', type=str, dest='calibFile2', help='Second file to open', required=True)
+    parser.add_argument('--calib1', type=str, dest='calibFileLeft', help='First file to open', required=True)
+    parser.add_argument('--calib2', type=str, dest='calibFileRight', help='Second file to open', required=True)
     args = parser.parse_args().__dict__
-    cameraType, file1, file2 = args["cameraTypes"], args["calibFile1"], args["calibFile2"]
-    #Initialise circle detector
+    cameraType, fileLeft, fileRight = args["cameraTypes"], args["calibFileLeft"], args["calibFileRight"]
+    # Initialise circle detector
     now = datetime.now()
     dt_string = now.strftime("%d-%m-%Y_%H-%M")
     saveDirectory = os.path.join("Results", cameraType + "_" + dt_string)
-    main(cameraType, saveDirectory)
+    main(cameraType, fileLeft, fileRight)
--- Python-Recalage/utils.py
+++ Python-Recalage/utils.py
@@ -27,14 +27,14 @@ def openCalibrationFile(filename):
 def scaleForHconcat(targetImage, referenceImage, scalingFactor = 1.0):
     # Scales images to have same height for hconcat side-by-side display
     # Preserves aspect ratio of target
-    targetRatio =  targetImage.shape[1] / targetImage.shape[0]
+    targetRatio = targetImage.shape[1] / targetImage.shape[0]
 
     # Calculates new dimensions based on ratio, referenceImage height and a potential scaling factor
-    referenceH, referenceW  = referenceImage.shape[0:2]
+    referenceH, referenceW = referenceImage.shape[0:2]
     targetDim = (int(referenceH * targetRatio * scalingFactor), int(referenceH * scalingFactor))
     referenceDim = (int(referenceW * scalingFactor), int(referenceH * scalingFactor))
     # Resizes images with calculated dimensions
-    targetImage = cv.resize(targetImage, targetDim)
-    referenceImage = cv.resize(referenceImage, referenceDim)
+    resizedTargetImage = cv.resize(targetImage, targetDim)
+    resizedReferenceImage = cv.resize(referenceImage, referenceDim)
     # Returns but also modifies images in place anyways
-    return targetImage, referenceImage
+    return resizedTargetImage, resizedReferenceImage
