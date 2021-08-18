bdb729ef39a1a5854b759b072b23d82f472125ea
Guillaume Jones <guillaume.jones@oramedical.ca>
2021-08-18 19:32:56 -0400

    Updated README and commented calibrate.py and calibrationHandler.py in PEP 8 style

--- Python-Recalage/calibrate.py
+++ Python-Recalage/calibrate.py
@@ -1,71 +1,86 @@
-import numpy as np
 import cv2 as cv
 import argparse
 from datetime import datetime
 import os
 # Local modules
-from videostream import selectStreams, StreamType
+from videostream import selectStreams
 from circledetector import CircleDetector
 from calibrationhandler import CalibrationHandler
 from circlegridfinder import CircleGridFinder
 from utils import stop
 
 def main(cameraType, saveDirectory):
-    ## Global calibration parameters
+    """Calibrate.py obtains intrinsic camera parameters using a grid pattern 
+    Parameters
+    ----------
+    cameraType : string
+        First character indicates type of camera that is to be calibrated
+    saveDirectory : string
+        Directory in which to save the calibration matrices and error image
+    """
+    # Global calibration parameters
     # Maximum number of checkerboard images to obtain
     initialCalibImages = 30
-    # Minimum number of checkerboard images to calculate matrices after removing outliers
+    # Minimum number of checkerboard images to calculate matrices from,
+    # after removing outliers
     minCalibImages = 27
     # Maximum error to tolerate on each point during calibration, in pixels
-    # Beyond this, grid is rejected as outlier
+    # Beyond this, entire grid is rejected as outlier
     maximumPointError = 3
-
     # Initialize circle detector for finding circle shapes in images
     circleDetector = CircleDetector()
-
     # Open stream based on camera index or height, see videostream.py
     stream, type = selectStreams(cameraType[0:1])
+    # Creates object that continually finds circle grids in images
+    gridFinder = CircleGridFinder(cameraType, [stream], [type], 
+        [circleDetector], initialCalibImages)
 
     # Initial message to user
     print("Press q to exit program.")
-
-    # Creates object that continually finds circle grids in images
-    gridFinder = CircleGridFinder(cameraType, [stream], [type], [circleDetector], initialCalibImages)
+    print("Taking grid pictures...")
 
     # Main capture and calibrate loop
-    print("Taking grid pictures...")
-    # This section asks the user to find initialCalibImages grids in each camera
-    takeMoreImages = True # Used to retake images if too many are removed during calibration
+    # This section asks the user to find initialCalibImages grids
+    takeMoreImages = True
     while takeMoreImages:
         gridFinder.start()
         # Repeats until enough images found in this stream
         while not gridFinder.finished:
+            # Checks that the stream is still open
             if stream.stopped:
-                stop("Camera disconnnected, not enough " + gridFinder.name + " frames taken for calibration.", [stream])
+                stop("Camera disconnnected, not enough " + gridFinder.name 
+                    + " frames taken for calibration.", [stream])
             else:
                 # Reads a new frame to display to user as GUI
                 frame = stream.read().copy()
-                gridFinder.drawCircles([frame]) # Large performance impact, displays circles detected in real time
-                gridFinder.drawOutlines([frame]) # Small performance impact, displays previously found grids as green lines
+                # Large performance impact, displays circles in real time
+                gridFinder.drawCircles([frame]) 
+                # Small performance impact, displays previously found grids
+                gridFinder.drawOutlines([frame]) 
                 # Indicates how many images remain to take in upper left
-                cv.putText(frame, 'Remaining images: ' + str(initialCalibImages - gridFinder.len()), (10, 50), cv.FONT_HERSHEY_SIMPLEX, 1, (250, 150, 0), 2)
+                cv.putText(frame, 'Remaining images: ' 
+                    + str(initialCalibImages - gridFinder.len()), 
+                    (10, 50), cv.FONT_HERSHEY_SIMPLEX, 1, (250, 150, 0), 2)
                 # Shows frame
                 cv.imshow('Calibration', frame)
-                # Waits 1 ms for user input, allowing user to quit main loop if 'q' is pressed
+                # Waits 1 ms for user input, allowing user to 
+                # quit main loop if 'q' is pressed
                 if cv.waitKey(1) == ord('q'):
                     stop("User exited program.", [stream])
         # All grids have now been found, proceeding to calibration
         # Gets frame size for this stream
-
         frameSize = frame[:, :, 0].shape[::-1]
-        # Creates Calibration object to handle cv.calibrateCamera() and error-checking
-        calibration = CalibrationHandler(gridFinder.objectPositions, gridFinder.allImagePositions[0], frameSize, minCalibImages, maximumPointError)
+        # Creates CalibrationHandler object to handle cv.calibrateCamera() 
+        # and error-checking
+        calibration = CalibrationHandler(gridFinder.objectPositions,
+            gridFinder.allImagePositions[0], frameSize, 
+            minCalibImages, maximumPointError)
+        # If the calibration succeeds
         if calibration.calibrate():
-            # If the calibration succeeds
-
             # Display matplotlib graphics of errors and saves to .png
             calibration.displayError(saveDirectory)
-            # Tries to save matrices to directory, can fail
+            # Tries to save matrices to directory, can fail if 
+            # wrong directory/file in use
             try:
                 calibration.writeToFile(saveDirectory)
             except:
@@ -77,14 +92,18 @@ def main(cameraType, saveDirectory):
 
     stop("Calibration successful.", [stream])
 
-
+# Standard argument parser implementation for command-line use
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', type=str, dest='cameraType', help='Kinect or FLIR', required=True)
+    # Selection of camera type with -c argument. 
+    # K, F or W are accepted (see utils, selectStreams)
+    parser.add_argument('-c', type=str, dest='cameraType', 
+        help='Kinect or FLIR', required=True)
     args = parser.parse_args().__dict__
     cameraType = args["cameraType"]
-    #Initialise circle detector
+    #Initialise directory with Date and Time in name
     now = datetime.now()
     dt_string = now.strftime("%d-%m-%Y_%H-%M")
     saveDirectory = os.path.join("Results", cameraType + "_" + dt_string)
+    # Starts true program
     main(cameraType, saveDirectory)
--- Python-Recalage/calibrationhandler.py
+++ Python-Recalage/calibrationhandler.py
@@ -6,75 +6,178 @@ import os
 from utils import NumpyEncoder
 
 class CalibrationHandler:
-    """Class for calculating and recalculating calibrations based on image and object points"""
-    def __init__(self, objectPositions, imagePositions, imageSize, minimumImages, maximumPointError = 10):
-        # Assignment
-        self.objectPositions, self.imagePositions, self.imageSize, self.minimumImages, self.maximumPointError = objectPositions, imagePositions, imageSize, minimumImages, maximumPointError        
+    """Calculates calibration and checks errors using points in image and world
+
+    Attributes
+    ----------
+    objectPositions : list of float points
+        Positions in 3D space of the circles of a grid. 
+        Can be relative (i.e. first circle is 0, 0)
+    imagePositions : list of float points
+        Positions in image (pixels) of grid circles
+    imageSize : int, int
+        Width and height of image in pixels
+    minimumImages : int
+        Minimum number of grids for calibration
+        If the number of grids drops below this, cancel calibration
+    maximumPointError : float/int
+        Maximum retroprojection error to tolerate on single points
+        If a point in a grid exceeds this, the grid (image) is rejected
+    calibrationDone : bool
+        Indicates if calibration has completed successfully
+        
+    Parameters
+    ----------
+    See attributes above.
+
+    Methods
+    -------
+    calibrate() 
+        Attempts to perform a complete calibration, including error checking
+    displayError(saveDirectory)
+        Prints error graphs from calibration and saves image
+    writeToFile(saveDirectory)
+        Saves intrinsic matrices in .json to saveDirectory
+    len()
+        Returns length of objectPositions, aka number of grids remaining
+    """
+    def __init__(self, objectPositions, imagePositions, imageSize, 
+                 minimumImages, maximumPointError = 10.0):
+        self.objectPositions = objectPositions
+        self.imagePositions = imagePositions
+        self.imageSize = imageSize
+        self.minimumImages = minimumImages
+        self.maximumPointError = maximumPointError
+        # Calibration marked as incomplete
         self.calibrationDone = False
-        # Index of popped positions during calculateErrors
-        self.indexesToPop = []
 
     def calibrate(self):
-        # Checks that there are sufficient images remaining to perform an adequate calibration
+        """Attempts to perform a complete calibration, including error checking
+        
+        Calibrate relies on cv.calibrateCamera from OpenCV to generate 
+        camera instrinsics from objectPoints and imagePoints. It thens uses _calculateErrors() to check if the instrinsics are good.
+
+        Returns
+        -------
+        calibrationDone : bool
+            True if calibration passes error checks
+            False if too many images are rejected and 
+            the calibration is impossible
+        """
+        # Checks that there are sufficient images remaining 
+        # to perform an adequate calibration
         if len(self.objectPositions) > self.minimumImages:
             # Calculates calibration parameters
-            self.retroprojectionError, self.cameraMatrix, self.distortion, self.rotation, self.translation = cv.calibrateCamera(self.objectPositions, self.imagePositions, self.imageSize, None, None, flags=cv.CALIB_RATIONAL_MODEL)
-            self.distortion = self.distortion[:,0:8] # Rational model uses 8 parameters, but 12 are returned (last 4 are 0)
+            (self.retroprojectionError, 
+            self.cameraMatrix, self.distortion, 
+            self.rotation, self.translation) = cv.calibrateCamera(
+                self.objectPositions, self.imagePositions, 
+                self.imageSize, None, None, 
+                flags=cv.CALIB_RATIONAL_MODEL)
+            # Rational model uses 8 params, but 12 are returned (last 4 are 0)
+            self.distortion = self.distortion[:,0:8] 
             # Checks for outliers using calibration parameters
             self.calibrationDone = self._calculateErrors()
         else:
             self.calibrationDone = False
-        # Returns False if there are too few images once outliers are removed, True if calibration succeeded
+
         return self.calibrationDone
 
     def _calculateErrors(self):
-        self.x, self.y, self.xError, self.yError, self.absError = [], [], [], [], []
+        """Checks calculated intrinsic parameters for errors
+
+        Performs a retroprojection calculation for every single grid.
+        If the error on a single grid point exceeds maximumPointError,
+        grid is rejected and a new calibration is asked for.
+
+        Returns
+        -------
+        bool
+            If no errors, returns True to signify good calibration
+            Otherwise, returns result of recursive call to calibrate() 
+            and _calculateErrors()
+            Allows True or False to propagate back to first calibrate() call
+        """
+        # Keeps track of errors for graphic display
+        self.x, self.y = [], []
+        self.xError, self.yError, self.absError = [], [], []
+        # Used to pop images with errors
+        indexesToPop = []
         redoCalibration = False
-        # Iterates through each image to calculate x and y errors of all corners
-        for index, imagePositionOld in enumerate(self.imagePositions):
-            # Reprojects objects to positions in image
-            imagePositionNew, _ = cv.projectPoints(self.objectPositions[index], self.rotation[index], self.translation[index], self.cameraMatrix, self.distortion)
-            imageHasOutliers = False
+        # Iterates through each image to calculate x and y errors 
+        # of all corners
+        for index, imagePosOld in enumerate(self.imagePositions):
+            # Reprojects 3D objects to 2D pixel positions in image
+            imagePosNew, _ = cv.projectPoints(
+                self.objectPositions[index], self.rotation[index], 
+                self.translation[index], self.cameraMatrix, self.distortion)
+            # Keeps track of errors for graphic display
             imageAbsErrors = []
-            # Iterates through each point to calculate more complicated absError (cannot be done directly through list)
-            for (oldPoint, newPoint) in zip(imagePositionOld, imagePositionNew):
+            # Iterates through each point to calculate more complicated 
+            # absError (cannot be done directly through list)
+            for (oldPoint, newPoint) in zip(imagePosOld, imagePosNew):
                 # Calculates error using L2 norm (distances squared)
                 error = cv.norm(oldPoint[0], newPoint[0], cv.NORM_L2)
                 if error > self.maximumPointError:
-                    # If error is too large, eliminates the image in which the error is found
-                    self.indexesToPop.append(index)
-                    # Indicates that image has outliers
-                    imageHasOutliers = True
-                    # Asks to recalculate calibration
+                    # If error is too large, eliminates the image 
+                    # in which the error is found
+                    indexesToPop.append(index)
+                    # Asks to recalculate calibration at the end
                     redoCalibration = True
                     # Does not need to check remaining points in this image
+                    # Also prevents errors from image from being saved
                     break
                 else:
                     # Adds error to list of errors to display at the end
                     imageAbsErrors.append(error)
-            if not imageHasOutliers:
+            # Keeps errors only if entire image is kept (no large errors)
+            # Otherwise, break statement will cause this to be skipped
+            else:
                 # Kept for graphic representation after calibration
                 self.absError.extend(imageAbsErrors)
-                self.x.extend(imagePositionOld[:,0,0].ravel())
-                self.y.extend(imagePositionOld[:,0,1].ravel())
-                self.xError.append(imagePositionNew[:,:,0].ravel() - imagePositionOld[:,:,0].ravel())
-                self.yError.append(imagePositionNew[:,:,1].ravel() - imagePositionOld[:,:,1].ravel())
+                self.x.extend(imagePosOld[:,0,0].ravel())
+                self.y.extend(imagePosOld[:,0,1].ravel())
+                # Calculates all xErrors/yErrors for image in single list
+                self.xError.append(
+                    imagePosNew[:,:,0].ravel() - imagePosOld[:,:,0].ravel())
+                self.yError.append(
+                    imagePosNew[:,:,1].ravel() - imagePosOld[:,:,1].ravel())
 
-        # Deletes all outlier frames, in reverse order to prevent dynamic modification of indices
+        # Deletes all outlier frames,
+        # in reverse order to prevent dynamic modification of indices
         for index in reversed(self.indexesToPop):
             self.imagePositions.pop(index)
             self.objectPositions.pop(index)
         self.indexesToPop = []
 
-        # Redoes calibration without the popped images
         if redoCalibration:
-            # Returns False if there are not enough images left at some iteration
+            # Performs calibrate() and _calculateErrors() recursively,
+            # without the popped images
             return self.calibrate()
         else:
-            # If no incorrect points detected, returns True to self.calibrate()
+            # If no incorrect points detected, returns True to calibrate()
             return True
 
     def displayError(self, saveDirectory):
+        """ Prints error graphs from calibration and saves image
+
+        Calibration and _calculateErrors generate two kinds of errors.
+        First are x and y errors for each point, which are used to generate
+        a scatter plot that can show missed outliers or biases in data.
+        Second are absolute total errors, which are used to generate a 
+        heatmap of errors to identify problematic areas of the image.
+        If errors are concentrated in the corners, it is a sign of faulty 
+        distortion coefficients.
+
+        Parameters
+        ----------
+        saveDirectory : string
+            Directory in which to save .png with error graphs
+
+        Returns
+        -------
+        None.     
+        """
         # Checks that error values exist to avoid crashing
         if self.calibrationDone:
             # Overall error printed to command line
@@ -83,27 +186,43 @@ class CalibrationHandler:
             # side-by-side axes that cover screen
             fig, (ax1, ax2) = plt.subplots(1,2)
             fig.set_size_inches(15, 8)
+
             # Scatter plot of relative errors
             scatterPlot = ax1.scatter(self.xError, self.yError)
             ax1.set_title('X and Y error for points in calibration')
             ax2.set_ylabel('Y error, pixels')
             ax2.set_xlabel('X error, pixels')
+
             # Intensity chart of errors based on position in image
             contourPlot = ax2.tricontourf(self.x, self.y, self.absError, 100, cmap='magma', extend='both', antialiased=False)
             ax2.set_title('Absolute errors across image pixels')
             colorBar = fig.colorbar(contourPlot)
             colorBar.ax.set_ylabel('Absolute error')
             plt.axis('scaled')
+            # .savefig must come before .show or saved .png will be blank
             plt.savefig(os.path.join(saveDirectory, 'ErrorCharts' + '.png'))
             plt.show()
-    def len(self):
-        return len(self.objectPositions)
 
-    def writeToFile(self, saveDirectory, filePrefix="Calib"):
+    def writeToFile(self, saveDirectory):
+        """ Saves intrinsic matrices in .json to saveDirectory
+
+        Creates a directory at the specified saveDirectory.
+        Prints cameraMatrix and distortion coefficients in .json
+        format to "Calib.json" within directory.
+
+        Parameters
+        ----------
+        saveDirectory : string
+            Directory in which to save .png with error graphs
+
+        Returns
+        -------
+        None.     
+        """
          # Creates filepath for results of calibration
         if not os.path.isdir(saveDirectory):
             os.mkdir(saveDirectory)
-        filename = os.path.join(saveDirectory, filePrefix + ".json")
+        filename = os.path.join(saveDirectory, "Calib.json")
         # Outputs calibration matrices to file
         with open(filename, 'w') as file:
             # Assigns labels to values to make JSON readable
@@ -111,3 +230,15 @@ class CalibrationHandler:
             # Uses NumpyEncoder to convert numpy values to regular arrays for json.dump
             json.dump(dumpDictionary, file, indent=4, cls=NumpyEncoder)
             print("Succesfully wrote calibration to file.")
+
+    def len(self):
+        """ Returns length of objectPositions, aka number of grids remaining
+
+        Convenience function
+
+        Returns
+        -------
+        length : int
+            Number of objectPositions remaining
+        """
+        return len(self.objectPositions)
