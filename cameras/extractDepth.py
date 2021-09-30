17c6e5164b7b31301b47f6398519083f50a3d654
olivierdesclaux <desclaux.olivier@gmail.com>
2021-09-30 13:00:48 -0400

    Cleaned file

--- cameras/extractDepth.py
+++ cameras/extractDepth.py
@@ -1,8 +1,6 @@
-import pyk4a as k4a
 import cv2
 from typing import Optional, Tuple
 import numpy as np
-import videostream
 
 def colorize(
         image: np.ndarray,
@@ -19,36 +17,6 @@ def colorize(
 
 
 def main():
-    cameras = 'K'
-    # stream, _ = videostream.selectStreams(cameras)
-    # stream = stream[0]
-    stream = videostream.KinectVideoStream()
-    while True:
-        capture = stream.read()
-        if capture.depth is not None:
-            cv2.imshow("Depth", colorize(capture.depth, (None, 5000), colormap=cv2.COLORMAP_BONE))
-            # cv2.imshow("Depth", capture.depth)
-        # if capture.ir is not None:
-        #     cv2.imshow("IR", colorize(capture.ir, (None, 500), colormap=cv2.COLORMAP_JET))
-        if capture.color is not None:
-            cv2.imshow("Color", capture.color)
-        if capture.transformed_depth is not None:
-            cv2.imshow("Transformed Depth", colorize(capture.transformed_depth, (None, 5000), cv2.COLORMAP_BONE))
-            # cv2.imshow("Transformed Depth", capture.transformed_depth)
-        if capture.transformed_color is not None:
-            cv2.imshow("Transformed Color", capture.transformed_color)
-        # if capture.transformed_ir is not None:
-        #     cv2.imshow("Transformed IR", colorize(capture.transformed_ir, (None, 500), colormap=cv2.COLORMAP_JET))
-
-        key = cv2.waitKey(10)
-        if key == ord("q"):
-            cv2.destroyAllWindows()
-            break
-        print("Transformed Depth", capture.transformed_depth.shape)
-        print("Color", capture.color.shape)
-        print("Depth", capture.depth.shape)
-
-
     pass
 
 
