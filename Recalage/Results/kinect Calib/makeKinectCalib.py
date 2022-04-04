0be360f6bf88a52f9150394a080c3e2e9d8508e7
Olivier Desclaux <desclaux.olivier@gmail.com>
2022-04-04 15:40:36 -0400

    Adding results used for stereocalibration finetuning

---
+++ Recalage/Results/kinect Calib/makeKinectCalib.py
@@ -0,0 +1,43 @@
+import os
+import numpy as np
+import json
+
+
+def main():
+    sets = os.listdir(".")
+    sets = [x for x in sets if os.path.isdir(x)]
+    mat = np.empty((len(sets), 3, 3))
+    dist = np.empty((len(sets), 5))
+    for i, set in enumerate(sets):
+        jsonFile = os.path.join(".", set, "kinectCalib.json")
+        with open(jsonFile) as f:
+            jsonDict = json.load(f)
+        mat[i, :, :] = jsonDict["CameraMatrix"]
+        dist[i, :] = jsonDict["DistortionCoefficients"][0]
+
+    mat_mean = np.mean(mat, axis=0)
+    dist_mean = np.mean(dist, axis=0)
+    filename = "kinectCalib.json"
+    with open(filename, 'w') as file:
+        # Assigns labels to values to make JSON readable
+        dumpDictionary = {
+            'Format': 'OpenCV', 'Model': 'Rational',
+            'CameraMatrix': mat_mean,
+            'DistortionCoefficients': dist_mean}
+        json.dump(dumpDictionary, file, indent=4, cls=NumpyEncoder)
+
+
+class NumpyEncoder(json.JSONEncoder):
+    """
+    # Encodes numpy arrays as normal lists. Otherwise, they cannot be encode to JSON. Used when invoking json.dump as
+    cls param
+    """
+
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+
+if __name__ == "__main__":
+    main()
